a205349e6f72cda0ec7b586fe1df812a6fc4a4bc
Elle <rjunelle1618@gmail.com>
2025-04-03 23:09:07 +0800

    Initial commit with the number guessing game

---
+++ guessing_game.py
@@ -0,0 +1,27 @@
+******************************
+  Welcome to the Guessing Game!
+******************************
+
+I have picked a number between 1 and 100. Can you guess it?
+Enter a number between 1 and 100: 50
+Too low! Try again.
+Enter a number between 1 and 100: 75
+Too high! Try again.
+Enter a number between 1 and 100: 63
+Too high! Try again.
+Enter a number between 1 and 100: 57
+Too high! Try again.
+Enter a number between 1 and 100: 53
+Congratulations! You guessed the number in 5 attempts.
+
+Would you like to play again? (y/n): y
+I have picked a number between 1 and 100. Can you guess it?
+Enter a number between 1 and 100: 50
+Too low! Try again.
+Enter a number between 1 and 100: 70
+Too high! Try again.
+Enter a number between 1 and 100: 60
+Congratulations! You guessed the number in 3 attempts.
+
+Would you like to play again? (y/n): n
+Thanks for playing! Goodbye.
